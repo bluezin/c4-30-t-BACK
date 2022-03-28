c0cb79fb7f9c32448cef00a1891d2d73837e73a7
yadira <yadiraco304@gmail.com>
2022-03-28 16:06:21 -0500

    adding model user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_action :authenticate_user!
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/auth/sign_in', to: "sessions#new"
+
+    get '/auth/sign_up', to: "registrations#new"
+    post '/auth/sign_up', to: "registrations#create"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
