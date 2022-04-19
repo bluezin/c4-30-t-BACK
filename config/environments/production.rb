8288ac47d1f4b6f328390b898caf6b2e718850bb
yadira <yadiraco304@gmail.com>
2022-04-18 23:11:06 -0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,12 +109,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'veride@gmail.com' }
   config.action_mailer.default_url_options = { host: 'country-app-v3.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'vercel.com',
+    domain:               'example.com',
     user_name:            'yadiraco304@gmail.com',
     password:             'yadi123',
     authentication:       'plain',
